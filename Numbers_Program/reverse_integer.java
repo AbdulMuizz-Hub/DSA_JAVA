fb4815953c97c07b5c8d607ac021abee4e739160
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-11-12 01:31:37 +0530

    initial commit

---
+++ Numbers_Program/reverse_integer.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class reverse_integer {
+     public static int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x = x / 10;
+            reversed = reversed * 10 + digit;
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner (System.in);
+
+       int n = sc.nextInt();
+       System.out.println(reverse(n));
+
+
+
+    }
+}
