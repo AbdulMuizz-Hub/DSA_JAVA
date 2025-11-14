a170bf0bac83fa7b2e16e45c49ea87011fe8f205
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-11-15 01:11:58 +0530

    updated

--- Array/Reverse_Array.java
+++ Array/Reverse_Array.java
@@ -16,7 +16,7 @@ public class Reverse_Array {
     public static void main(String[] args) {
         int numbers[] = {2,4,6,8,10,};
 
-        reverse(numbers);
+        reverse(numbers);   // calling funtion
         for(int i = 0; i < numbers.length; i++){
             System.out.print(numbers[i] +" ");
         }
