35fc6cfe058c6ba72721f1597e7a62f185588287
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-11-14 23:34:19 +0530

    initial

--- Array/array.java
+++ Array/array.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class array{
     public static void main (String arg[]){
         int marks[] = new int [50];
-        Scanner var1 = new Scanner (System.in);
+        Scanner sc = new Scanner (System.in);
 
-        marks[0] = var1.nextInt();
-        marks[1] = var1.nextInt();
-        marks[2] = var1.nextInt();
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
 
         System.out.println("Physics = " + marks[0]);
         System.out.println("Chemistry = " + marks[1]);
