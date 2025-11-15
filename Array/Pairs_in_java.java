a4e00f7a6a9822c93d7cf3c2ea3f622650606aff
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-11-15 20:33:02 +0530

    updated

--- Array/Pairs_in_java.java
+++ Array/Pairs_in_java.java
@@ -1,13 +1,16 @@
 public class Pairs_in_java{
     
     public static void printPairs(int numbers[]){
+        int Total_pair = 0 ;
         for(int i = 0; i<numbers.length;i++){
             int current = numbers[i];
             for(int j= i+1 ; j<numbers.length; j++){
                 System.out.print("(" + current + "," + numbers[j] + ")");
+                Total_pair++;
             }
             System.out.println();
         }
+        System.out.println("Total Number of pairs = " + Total_pair);
 
     }
     
