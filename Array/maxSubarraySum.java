4d3ba289af654915cdf8fc7ce0e2428cd9548620
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-11-17 23:23:59 +0530

    updated

--- Array/maxSubarraySum.java
+++ Array/maxSubarraySum.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class maxSubarraySum {
 
-    public static void maxSubarraySum(int numbers[]){     
+    public static void maxSubarraySum(int numbers[]){      // function for maxSubarraysum
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
 
@@ -18,11 +18,11 @@ public class maxSubarraySum {
                 }
             }
         }
-        System.out.println(" maximum subarray sum = " + maxSum);
+        System.out.println(" maximum subarray sum = " + maxSum);  // to print max subarray
     }
     public static void main (String args[]){
         int numbers[] = {1,-2,6,-1,3};
-        maxSubarraySum(numbers); 
+        maxSubarraySum(numbers);  // calling maxSubarraySum
 
     }
 }
