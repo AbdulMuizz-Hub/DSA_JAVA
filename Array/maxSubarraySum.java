2b90053e9457a2f05c8fb65519d96964f4295dd8
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-11-17 23:36:12 +0530

    updated

--- Array/maxSubarraySum.java
+++ Array/maxSubarraySum.java
@@ -4,6 +4,7 @@ public class maxSubarraySum {
     public static void maxSubarraySum(int numbers[]){      // function for maxSubarraysum
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
+        int start = 0 , end = 0;
 
         for (int i = 0; i < numbers.length; i++){
             for (int j = i ; j < numbers.length; j++){
@@ -12,13 +13,21 @@ public class maxSubarraySum {
                       // calculate sum of  subarrays
                       currSum += numbers[k];
                 }
-                System.out.println(currSum);
+                // System.out.println(currSum);
                 if (maxSum < currSum){
                     maxSum = currSum;
+                    start = i;
+                    end = j;
                 }
             }
         }
         System.out.println(" maximum subarray sum = " + maxSum);  // to print max subarray
+        // to print subarray which has a max subarraysum
+        System.out.print("Subarray: [ ");
+        for(int x = start; x <= end; x++){
+            System.out.print(numbers[x] + " ");
+        }
+        System.out.println("]");
     }
     public static void main (String args[]){
         int numbers[] = {1,-2,6,-1,3};
