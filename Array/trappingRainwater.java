636990a9375a056af1c7359a5ec59233824e3df8
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-11-27 00:37:09 +0530

    updated

--- Array/trappingRainwater.java
+++ Array/trappingRainwater.java
@@ -2,18 +2,21 @@ public class trappingRainwater {
     public static int trap(int[] height) { // creating function for trapped water
         int n = height.length;
 
+        // calculate left max boundary
         int leftMax[] = new int [n];
         leftMax[0] = height[0];
         for (int i=1; i<n ; i++){
             leftMax[i] = Math.max(height[i], leftMax[i-1]);
         }
 
+        // calculate max right boundary
         int rightMax[] = new int [n];
         rightMax[n-1] = height[n-1];
         for(int i = n-2 ; i>=0 ; i--){
             rightMax[i] = Math.max(height[i] , rightMax[i+1]);
         }
 
+        // calculating trapped water
         int width = 1;
         int trappedWater = 0;
         for ( int i=0; i<n; i++){
