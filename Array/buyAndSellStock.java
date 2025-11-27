8e5a09d752b2a676d6aaea0e20077777d5264cab
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-11-27 23:59:33 +0530

    initial commit

--- Array/buyAndSellStock.java
+++ Array/buyAndSellStock.java
@@ -17,6 +17,6 @@ public class buyAndSellStock{
 
     public static void main (String args[]){
         int[] prices = {7,1,5,3,6,4};
-        System.out.println(maxProfit(prices));
+        System.out.println(maxProfit(prices)); // calling function
     }
 }
