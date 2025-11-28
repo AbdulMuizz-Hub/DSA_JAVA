a7de169a3f2e1dafd263d8156da5f0904fa5b2c9
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-11-29 03:33:15 +0530

    updated

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -12,7 +12,7 @@ public class bubbleSort {
             }
         }
     }
-
+    // printing function
     public static void printArr(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i] + ",");
