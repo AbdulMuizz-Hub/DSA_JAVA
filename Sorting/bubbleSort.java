eab2c7a1c55012c81c77da6b4f95440f6c47aff7
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-12-01 23:44:33 +0530

    updated

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -4,7 +4,7 @@ public class bubbleSort {
         for(int turn=0; turn<arr.length-1; turn++){  // logic for bubble sort
             for(int j=0; j<arr.length-1-turn; j++){
                 if(arr[j] > arr[j+1]){
-                    // swap
+                    // swap the numbers
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
