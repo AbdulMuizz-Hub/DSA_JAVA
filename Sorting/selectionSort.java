2e6987610a678af67ef6b10ae64ad0a45937b1c6
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-12-01 23:59:34 +0530

    updated

--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -15,7 +15,7 @@ public class selectionSort {
             arr[i] = temp;
         }
     }
-
+    // printing function
     public static void printArray(int arr[]){
         for(int i=0; i<arr.length;i++){
              System.out.print(arr[i] + ",");
