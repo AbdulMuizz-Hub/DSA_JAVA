44a4a71a7699987fe61b67a6b2b21af15842515c
Abdul_Muizz <abdulmuizz7262@gmail.com>
2025-12-05 23:55:55 +0530

    updated

--- Array/buyAndSellStock.java
+++ Array/buyAndSellStock.java
@@ -4,7 +4,7 @@ public class buyAndSellStock{
         int buyPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
 
-        for(int i = 0; i < prices.length; i++){
+        for(int i = 0; i < prices.length; i++){  // logic
             if(buyPrice < prices[i]){
             int profit = prices[i] - buyPrice;
             maxProfit = Math.max(maxProfit, profit);
